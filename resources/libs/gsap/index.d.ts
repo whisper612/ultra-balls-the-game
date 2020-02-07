aab1676336d7676f9c3ffad5526bfb9a1c764694
Whisper612 <mr.waylander@bk.ru>
2020-02-07 18:06:08 +0700

    GSAP added

---
+++ resources/libs/gsap/index.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for GSAP 1.20.6
+// Project: http://greensock.com/
+// Definitions by:  VILIC VANE <https://vilic.github.io/>,
+//                  Robert S <https://github.com/codebelt>,
+//                  Richard Fox <https://github.com/ProbablePrime>,
+//                  Philip Bulley <https://github.com/philipbulley>,
+//                  Leonardo Melo <https://github.com/leomeloxp>
+//                  Adem Hodzic <https://github.com/AdemHodzic>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="animation.d.ts"/>
+/// <reference path="ease.d.ts"/>
+/// <reference path="tweenConfig.d.ts"/>
+/// <reference path="tween.d.ts"/>
+/// <reference path="timeline.d.ts"/>
+/// <reference path="plugins.d.ts"/>
+
+declare module "gsap" {
+    export = gsap;
+}
